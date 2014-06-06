ff56d0ddf3179b23c2a429d09cfcc4fe01663ced
Justin <lorenzon@gmail.com>
2014-06-06 17:27:45 +0100

    work on incremental saves

--- index.js
+++ index.js
@@ -2,6 +2,45 @@ var http = require('http');
 var querystring = require('querystring');
 var request = require('request');
 
+var createOrUpdate = function(obj) {
+  if(typeof obj._id === 'undefined')
+    obj._id = obj.uuid;
+  
+  var requestSettings = {
+    "uri": "http://dev.lomis.ehealth.org.ng:5984/offline_sms_alerts/",
+    "json": obj
+    "method": "POST"
+  };
+  //try to post
+  request(requestSettings, function(err, res, body) {
+    //if couch returns 409 conflict & rev
+    if(res === 409)
+    {
+      //take uuid and revision and create PUT
+      requestSettings.method = "PUT";
+      requestSettings.json._rev = body._rev;
+      request(requestSettings, function(err, res, body) {
+        if(err !== null)
+          console.log(err);
+        //console.log(res);
+        console.log(body);
+      });
+    }
+
+    if(err !== null)
+      console.log(err);
+    //console.log(res);
+    console.log(body);
+  });
+  
+  request(requestSettings, function(err, res, body) {
+      if(err !== null)
+        console.log(err);
+      //console.log(res);
+      console.log(body);
+  });
+}
+
 http.createServer(function (req, res) {
 
   var requestMsgBody = '';
@@ -18,17 +57,8 @@ http.createServer(function (req, res) {
         //parse POST message body to json
         var decodedMsg = querystring.parse(requestMsgBody);
         console.log(decodedMsg.content);
-        request({
-          "uri": "http://dev.lomis.ehealth.org.ng:5984/offline_sms_alerts/",
-          "json": JSON.parse(decodedMsg.content),
-          "method": "POST"
-        }, function(err, res, body)
-        {
-          if(err !== null)
-            console.log(err);
-          //console.log(res);
-          console.log(body);
-        });
+        var obj = JSON.parse(decodedMsg.content);
+        createOrUpdate(obj);
       }
       res.end('reply to request: sms sent to server. \n');//reply sent to client/could be sms to be resen
     });
