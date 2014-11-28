93306eeb6eeac1a43d42e8f9b5b015b7f655c91e
femi oni <freefony@gmail.com>
2014-11-28 09:46:38 +0100

    offline ledger sms submission

--- libs/storage.js
+++ libs/storage.js
@@ -66,6 +66,59 @@ this.createOrUpdate = function(dbName, record){
             couchResponse[key] = record[key];
           }
         }
+        if(dbName=== "bundle"){
+            if(record.id){
+              if(!couchResponse.bundleLines || (!Array.isArray(couchResponse.bundleLines))){
+                couchResponse.bundleLines = [];
+              }
+              if(record.id){
+                var idExist = false;
+                couchResponse.bundleLines.forEach(function(line){
+                  if(line.id === record.id){
+                    idExist =  record.id;
+                  }
+                });
+                if(!idExist){
+                  couchResponse.bundleLines.push({id : record.id});
+                  idExist = record.id;
+                }
+                couchResponse.bundleLines.forEach(function(line){
+                   if(line.id === idExist){
+                     if(record.bno && (!line.batchNo)){
+                       line.batchNo = record.bno;
+                     }
+                     //if(record.product)
+                     var keys = Object.keys(record);
+                     keys.forEach(function(key){
+                       if(key === "vvm"){
+                         if(!line.VVMStatus) line.VVMStatus = {};
+                         var vvm = key.split(",");
+                         vvm.forEach(function(v){
+                            var vvm = v.split(":");
+                           line.VVMStatus[vvm[0]] = vvm[1];
+                         })
+
+                       }else {
+                         line[key] = record[key];
+                       }
+                     })
+                   }
+                });
+
+              }else{
+                var keys = Object.keys(record);
+                keys.forEach(function(key){
+                  if(key === "sf"){
+                    couchResponse["sendingFacility"] = record[key];
+                  }else if(key === "rf"){
+                    couchResponse["receivingFacility"] = record[key];
+                  }else{
+                    couchResponse[key] = record[key];
+                  }
+                })
+              }
+            }
+          }
         //set updated couchResponse as json doc to post to server.
         requestSettings.json = couchResponse;
 
@@ -88,6 +141,35 @@ this.createOrUpdate = function(dbName, record){
               delete record.ppId;
               delete record.quantity;
             }
+          }
+          if(record.db === "bundle"){
+            if(!record.bundleLines) record.bundleLines = [];
+            if(record.bno){
+              if(record.vvm){
+                var VVMStatus = {};
+                var vvm = key.split(",");
+                vvm.forEach(function(v){
+                  var vvm = v.split(":");
+                  VVMStatus[vmm[0]] = vvm[1];
+                })
+                record.bundleLines.push({
+                  id : record.id,
+                  batchNo: record.bno,
+                  VVMStatus: VVMStatus
+                });
+              }else{
+                var lineKeys = Object.keys(record);
+                lineKeys.forEach(function(key){
+                  var obj = {};
+                  obj[key] = record[key];
+                  //obj.id = record.id;
+                  //obj.batchNo = record.bno || '';
+                  record.bundleLines.push(obj)
+                })
+              }
+
+            }
+
           }
           //save record as a new doc.
           request(requestSettings, function (err, res, body) {
