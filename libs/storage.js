7a478499d0ec7a8d6714bed7524a13837a2a3c2f
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-06-08 17:05:25 +0100

    move configuration details into config/app.json

--- libs/storage.js
+++ libs/storage.js
@@ -1,11 +1,12 @@
 var q = require("q");
 var request = require('request');
 
-var BASE_URI = "http://dev.lomis.ehealth.org.ng:5984/";
+var config = require('konfig')();
+
+var BASE_URI =  config.app.DB_URL;
 
 this.FACILITY = "facilities";
 this.PRODUCT_TYPES = "product_types";
-this.CCU_PROFILE = "ccei";
 this.CONTACTS = "contacts";
 this.STOCK_OUT = "stock_out";
 this.CCU_BREAKDOWN = "ccu_breakdown";
@@ -13,7 +14,7 @@ this.OFFLINE_SMS_ALERTS = "offline_sms_alerts";
 
 this.getRecord = function(dbName, uuid){
   var deferred = q.defer();
-  var URI = BASE_URI+dbName+"/" + uuid;
+  var URI = BASE_URI + dbName + "/" + uuid;
   var opts = {
     "uri": URI,
     "method": "GET"
@@ -30,7 +31,7 @@ this.getRecord = function(dbName, uuid){
 
 this.createOrUpdate = function(dbName, record){
   var deferred  = q.defer();
-  var URI = BASE_URI+dbName+"/";
+  var URI = BASE_URI + dbName + "/";
 
   if (typeof record._id === 'undefined') {
     record._id = record.uuid;
