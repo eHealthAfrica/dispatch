d521a9fdf4dfbab5c568798a0ded88c7bdfc6672
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-08-04 14:24:10 +0100

    add test for docConverter.addTo(), hashBy() and toStockOut()

--- test/libs/doc-converter.spec.js
+++ test/libs/doc-converter.spec.js
@@ -144,6 +144,101 @@ describe('docConverter', function() {
 			expect(result).to.equal(false);
 		});
 
+		it('Should return FALSE if doc.db is "stockcount" and doc.unopened is not defined', function(){
+			var doc = {
+				db: 'stockcount',
+				uuid: '37a0e46e-efe8-44bc-f525-af373d4b3122',
+				_id: '37a0e46e-efe8-44bc-f525-af373d4b3122',
+				isComplete: 1,
+				countDate: '2015-07-29T23:00:00.000Z',
+				facility: 'FACILITY_ID',
+				created: '2015-08-03T13:01:49.276Z',
+				modified: '2015-08-03T13:01:49.276Z'
+			};
+
+			var result = docConverter.isComplete(doc);
+			expect(result).to.equal(false);
+		});
+
+		it('Should return TRUE if doc.db is "stockcount" and doc.unopened is defined', function(){
+			var doc = {
+				db: 'stockcount',
+				uuid: '37a0e46e-efe8-44bc-f525-af373d4b3122',
+				_id: '37a0e46e-efe8-44bc-f525-af373d4b3122',
+				isComplete: 1,
+				countDate: '2015-07-29T23:00:00.000Z',
+				facility: 'FACILITY_ID',
+				unopened: {
+					'V1': 200,
+					'V2': 0,
+					'V3': 200,
+					'V4': 100,
+					'V5': 200,
+					'v6': 100
+				},
+				created: '2015-08-03T13:01:49.276Z',
+				modified: '2015-08-03T13:01:49.276Z'
+			};
+
+			var result = docConverter.isComplete(doc);
+			expect(result).to.equal(true);
+		});
+
+	});
+
+	describe('hashBy', function() {
+
+		it('Should hash an Array into key value map by given property', function(){
+			var list = [ { _id: '1234' }, { _id: '1234' }, { _id: '3245' } ];
+			var hashMap = docConverter.hashBy(list, '_id');
+			var value = hashMap['1234'];
+			assert.isDefined(value);
+
+			var value2 = hashMap['3245'];
+			assert.isDefined(value2);
+		});
+
+	});
+
+
+	describe('addTo', function(){
+
+		it('Should NOT add UNDEFINED elements to list', function() {
+			var emptyList = [];
+			emptyList = docConverter.addTo(emptyList);
+			expect(emptyList.length).to.equal(0);
+		});
+
+		it('Should add DEFINED elements', function() {
+			var emptyList = [];
+			var elem1 = { db: 'test', _id: '123-XX-YY-ZZ' };
+			var elem2 = { db: 'test2', _id: '562-XX-YY-ZZ' };
+
+			emptyList = docConverter.addTo(emptyList, elem1);
+			emptyList = docConverter.addTo(emptyList, elem2);
+
+			expect(emptyList.length).to.equal(2);
+		});
+
+	});
+
+	describe('toStockOut', function () {
+
+		it('Should set _id property', function(){
+			var doc = {
+				db: 'stock_out',
+				uuid: '1234567890-1926177',
+				facility: 'Test Facility',
+				created: new Date().toJSON(),
+				productType: 'BCG'
+			};
+			var facility = { name: 'Test HP', _id: '123-KKL' };
+			var productType = { name: 'BCG', _id: '321-kLS-a32' };
+			assert.isUndefined(doc._id);
+			var result = docConverter.toStockOut(doc, facility, productType);
+			assert.isDefined(result._id);
+		});
+
 	});
 
 });
